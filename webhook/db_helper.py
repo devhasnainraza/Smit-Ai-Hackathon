8b3ff656bb053c1ddeadaf564d32509ab794830b
Muhammad Hasnain <developerhasnainraza@gmail.com>
2025-09-15 13:13:46 +0500

    Update db_helper.py

--- webhook/db_helper.py
+++ webhook/db_helper.py
@@ -1,10 +1,13 @@
 import re
 import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv('.env', override=True)
 
 MONGODB_URI = os.getenv("MONGODB_URI")
 if not MONGODB_URI:
-raise RuntimeError("MONGODB_URI is not set")
+    raise RuntimeError("MONGODB_URI is not set")
 client = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
 db = client["food_database"]
 
